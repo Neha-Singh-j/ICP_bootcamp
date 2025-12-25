481a68a59e2f9439c86e151e4566295e701abc39
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2025-12-25 18:48:53 +0530

    Time: 2 ms (74.21%) | Memory: 43.8 MB (24.46%) - LeetSync

---
+++ 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int distMoney(int money, int children) {
+
+        if (money < children) return -1;
+        if (money == 4 && children == 1) return -1;
+
+        int rem = money - children; //given 1 to each children then remaining money
+        int c = Math.min(children, rem / 7); 
+
+        if (c == children && rem - c * 7 > 0) {  //everyone get 8
+            c--;
+        }
+        if (rem - c * 7 == 3 && children - c == 1) { //avodiing givign 4 
+            c--;
+        }
+        //greedy result 
+        return Math.max(0, c);
+    }
+}
