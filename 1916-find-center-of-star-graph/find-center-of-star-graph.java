050246b2e375ca6876b14de6856cc0a1a2689daa
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-01 11:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 110.8 MB (61.43%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        //conditons is defined 1 is common in all then its comoon also in first 2 nodes....just return 
+        if(edges[0][1]==edges[1][0] || edges[0][1]==edges[1][1]){
+            return edges[0][1];
+        }
+        return edges[0][0];
+    }
+}
