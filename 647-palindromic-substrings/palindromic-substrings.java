482dd5238a174aa14fa056f45cbecdf377385d50
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-04 11:46:53 +0530

    Time: 7 ms (44.56%) | Memory: 43.1 MB (44.47%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,49 +1,25 @@
 class Solution {
     public int countSubstrings(String s) {
-        int c=0;
+        return count(s);
+    }
+
+    public int count(String s){
+        // odd length
+        int odd=0;
         for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(pal(s.substring(i,j+1))) c++;
+            for(int j=0;i-j>=0 && i+j<s.length();j++){
+                if(s.charAt(i-j)!=s.charAt(i+j)) break;
+                odd++;
             }
         }
-        return c;
-    }
-    public static boolean pal(String s){
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-                i++;
-                j--;
-            
+        // even
+        int even=0;
+        for(double i=0.5;i<s.length();i++){
+            for(double j=0.5;i-j>=0 && i+j<s.length();j++){
+                if(s.charAt((int)(i-j))!=s.charAt((int)(i+j))) break;
+                even++;
+            }
         }
-        return true;
+        return odd+even;
     }
-    public  int Count_Palindromic(String s) {
-		int odd = 0;
-		// odd
-		for (int axis = 0; axis < s.length(); axis++) {
-			for (int orbit = 0; axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
-				if (s.charAt(axis - orbit) != s.charAt(axis + orbit)) {
-					break;
-				}
-				odd++;
-				System.out.println(s.substring(orbit,axis));
-			}
-		}
-		// even
-		int even=0;
-		for (double axis = 0.5; axis < s.length(); axis++) {
-			for (double orbit = 0.5; axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
-				if (s.charAt((int) (axis - orbit)) != s.charAt((int) (axis + orbit))) {
-					break;
-				}
-				even++;
-				int c=(int)(axis);
-				System.out.println(s.substring((int)orbit,c));
-			}
-		}
-		return odd+even;
-
-	}
 }
