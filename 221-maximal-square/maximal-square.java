6aa15eaaef52ddc87dd592aa44d42ac44b3264f5
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-05 11:00:04 +0530

    Time: 9 ms (36.42%) | Memory: 71 MB (38.53%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,48 +1,30 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-       int[] arr=new int[matrix[0].length];
-        int ans=0;
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        
+        int curr=Integer.MIN_VALUE;
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]=='0'){
-                    arr[j]=0;
-                }else{
-                    arr[j]++;
-                }
+                curr=Math.max(curr,count(matrix,i,j,dp));
             }
-            ans=Math.max(ans,Largest_hist(arr));
         }
-        return ans;
+        return curr*curr;
     }
-    public static int Largest_hist(int[] arr) {
-		Stack<Integer> st=new Stack<>();
-		int ans=0;
-		for(int i=0;i<arr.length;i++) {
-			while(!st.isEmpty() && arr[i]<arr[st.peek()]) {
-				int h = arr[st.pop()];
-                int r = i;
-                int l = st.isEmpty() ? -1 : st.peek();
-
-                int width = r - l - 1;
-                int side = Math.min(h, width);
-
-                ans = Math.max(ans, side * side);
-			}
-				
-			st.push(i);
-		}
-		int r=arr.length;
-		 while (!st.isEmpty()) {
-            int h = arr[st.pop()];
-            int l = st.isEmpty() ? -1 : st.peek();
-
-            int width = r - l - 1;
-            int side = Math.min(h, width);
-
-            ans = Math.max(ans, side * side);
-        
-		}
-		return ans;
-	}
-
+    public static int count(char[][]arr, int i, int j,int[][]dp){
+        if(i>=arr.length ||i<0 || j<0 || j>=arr[0].length) return 0;
+        if(arr[i][j]=='0') return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int a=count(arr,i-1,j-1,dp);
+        int b=count(arr,i-1,j,dp);
+        int c=count(arr,i,j-1,dp);
+        return dp[i][j]=Math.min(Math.min(a,b),c)+1;
+    }
 }
