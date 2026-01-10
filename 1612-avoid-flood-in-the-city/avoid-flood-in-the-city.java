f29a56aecbfa22668d53bcdec8dcd972c3cf1c54
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-10 16:20:54 +0530

    Time: 95 ms (37.16%) | Memory: 164.4 MB (27.43%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        int n=rains.length;
+         int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        Map<Integer, Integer> prev = new HashMap<>();
+        TreeSet<Integer> dry = new TreeSet<>(); //get dry days in shorted manner
+
+        for (int i = 0; i < n; i++) {
+            if (rains[i] == 0) {
+                dry.add(i);
+                ans[i] = 1;
+            } else {
+                int lake = rains[i];
+                if (prev.containsKey(lake)) {
+                    Integer dryDay = dry.ceiling(prev.get(lake));
+                    if (dryDay == null) return new int[0];
+
+                    ans[dryDay] = lake;
+                    dry.remove(dryDay);
+                }
+                prev.put(lake, i);
+                ans[i] = -1;
+            }
+        }
+        return ans;
+    }
+}
