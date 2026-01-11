da18f967855a5c50d5efe128a588097309c7bebd
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-11 21:18:58 +0530

    Time: 2 ms (85.17%) | Memory: 52.4 MB (46.20%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        return reach(arr,start);
+    }
+    public boolean reach(int[]arr, int i){
+        if(i<0 || i>=arr.length) return false;
+        if(arr[i]==0) return true;
+        //mark visited otherwise recursion will not end
+        if (arr[i] < 0) return false;   // already visited
+
+        int jump = arr[i];
+        arr[i] = -arr[i]; 
+        boolean a=reach(arr,i+jump);
+        boolean b=reach(arr,i-jump);
+        return a||b;
+    }
+}
