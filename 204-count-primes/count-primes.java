d038b8537090186084cf10d6d869b13e7438011b
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-13 14:28:07 +0530

    Time: 99 ms (57.56%) | Memory: 71.9 MB (5.23%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        int[] ans=new int[n];
+        ans[0]=ans[1]=1; //not prime
+        for(int i=2;i*i<n;i++){
+            if(ans[i]==0){ //check is it composite
+                for(int j=2;i*j<n;j++){
+                    ans[i*j]=1;
+                }
+            }
+        }
+        int c=0;
+        for(int i:ans){
+            if(i==0) c++;
+        }
+        return c;
+
+    }
+}
