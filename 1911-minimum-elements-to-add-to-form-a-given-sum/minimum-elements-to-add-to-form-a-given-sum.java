e4d6ee698350a5f33729a3435ae0ab61981d9e34
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-01-16 21:50:49 +0530

    Time: 1 ms (100.00%) | Memory: 88.3 MB (27.59%) - LeetSync

---
+++ 1911-minimum-elements-to-add-to-form-a-given-sum/minimum-elements-to-add-to-form-a-given-sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum=0;
+        for (int x : nums) {
+            sum += x;
+        }
+        
+        long d = Math.abs(goal - sum);
+        if (d == 0) return 0;
+        return (int) ((d + limit - 1) / limit);
+    }
+}
